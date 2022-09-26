4d77242fab6ed75160abf15e6cfb26695ef7452e
comet1e <hilmansoccer@gmail.com>
2022-09-26 16:17:11 +0800

    add 07_tempConversion/tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
+const ftoc = function(inputF) {
 
+   resultF = (inputF - 32)* (5/9);
+  // return Math.round((inputF - 32)* (5/9));  ---- this wont give me output to 1 dp.
+  // return Number.(((inputF - 32)* (5/9)).toFixed(1));
+  
+  return parseFloat(resultF.toFixed(1));
+  // the toFixed function will output a string format.
+  // parseFloat is to convert the final output to number format.
+  
 };
 
-const ctof = function() {
-
+const ctof = function(inputC) {
+  resultC = ((inputC * (9/5))+32);
+  //return Math.round((inputC * (9/5))+32);
+  // return Number.(((parseFloat(inputC) * (9/5))+32).toFixed(1));
+  return parseFloat(resultC.toFixed(1));
 };
 
 // Do not edit below this line
@@ -11,3 +22,4 @@ module.exports = {
   ftoc,
   ctof
 };
+
